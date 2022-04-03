f674d32512f41833549b5027e03b8520c9fcc4c8
zzpapy <gregory.pace@hotmail.fr>
2022-04-03 18:36:15 +0200

    search actor

--- assets/components/Actor.js
+++ assets/components/Actor.js
@@ -4,7 +4,7 @@ import React from 'react';
      return (
      <div key={id} id={id} className="card homeConatainer film col-md-4" onClick={onClick}>         
         <p key={id} className="test"id={id}>{name}</p>
-        <div key={id} className="test"id={id}>
+        <div key={id} className="test"id={id}> 
         <img id={id} className="imgPoster"src={poster} />
         </div>
      </div>
