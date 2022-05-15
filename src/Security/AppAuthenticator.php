a5bfa49876ed2fa3bd8416d3b6c13ad42d2d3d7a
zzpapy <gregory.pace@hotmail.fr>
2022-05-15 11:10:48 +0200

    redirect login

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -50,7 +50,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_sas'));
+        return new RedirectResponse($this->urlGenerator->generate('app_phrase'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
